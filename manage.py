252a295237b549a6a7ef7133d1af45bd4ae962a7
Paul Xue <xuepaul.xp@gmail.com>
2012-08-03 13:44:00 -0400

    clean up manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,10 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-#from flask.ext.script import Manager, Server
 from flask.ext.actions import Manager
 from flasklog import app
 
 manager = Manager(app, default_server_actions=True)
-
-#manager.add_command('runserver', Server(
-    #use_debugger = True,
-    #use_reloader = True,
-    #host = '0.0.0.0',)
-#)
 
 if __name__ == "__main__":
     manager.run()
