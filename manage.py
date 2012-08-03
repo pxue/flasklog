6383f8544422bc4b4b2f78d8f58d4821f73db774
Paul Xue <xuepaul.xp@gmail.com>
2012-08-03 02:44:20 -0400

    using flask-actions for gunicorn integration

--- manage.py
+++ manage.py
@@ -1,16 +1,17 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask.ext.script import Manager, Server
+#from flask.ext.script import Manager, Server
+from flask.ext.actions import Manager
 from flasklog import app
 
-manager = Manager(app)
+manager = Manager(app, default_server_actions=True)
 
-manager.add_command('runserver', Server(
-    use_debugger = True,
-    use_reloader = True,
-    host = '0.0.0.0',)
-)
+#manager.add_command('runserver', Server(
+    #use_debugger = True,
+    #use_reloader = True,
+    #host = '0.0.0.0',)
+#)
 
 if __name__ == "__main__":
     manager.run()
